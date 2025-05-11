be00eb5bf07e3232fd73d45a5643b19fa2caa453
DaniSoler09 <soler092811@gmail.com>
2025-05-11 14:51:19 -0500

    JS-Updated

---
+++ README.md
@@ -0,0 +1,16 @@
+# BIT- Academia De Tenis La Rana. 
+
+### Academia de Tenis La Rana es un proyecto en desarrollo que tiene como objetivo proporcionar a sus miembros una plataforma web intuitiva. Esta plataforma facilitará el acceso a información general y fomentará una interacción activa con el personal de la academia.
+
+## El desarrollo de la plataforma web de la Academia de Tenis La Rana se está llevando a cabo utilizando las siguientes tecnologías
+* HTML
+* CSS
+* JavaScript
+
+## Figma.
+
+![](C:\Users\Soler\Downloads\LA RANA ACADEMIA)
+
+#### Diseño y Desarrollo: Daniel Ramirez Soler
+
+[def]: C:\Users\Soler\Downloads
---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let nombre = document.getElementById("nombre");
+let email = document.getElementById("email");
+let telefono = document.getElementById("telefono");
+let mensaje = document.getElementById("mensaje");
+
+let boton = document.getElementById("boton");
+
+let imprimir =document.getElementById("imprimir")
+imprimir.style = "display: none;";
+boton.addEventListener("click", function(){
+    imprimir.innerHTML = nombre.value + " " + email.value + " " + "con numero de telfono " + telefono.value + " su mensaje " + mensaje.value + "ha sido enviado.";
+    imprimir.style = "display: block; background-color: #4e8a60; color: #ffffff;" ;
+    nombre.value = "";
+    email.value = "";
+    telefono.value = "";
+    mensaje.value = "";
+})
